b7ea29d244f05a31396a5ea14368dd6a698fe657
Tejas Patil <139064001+Tejas-webdeveloper@users.noreply.github.com>
2023-10-31 10:16:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,49 +1,155 @@
+//AXIOS GLOBALS
+axios.defaults.headers.common['X-Auth-Token']=
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+
+
+
 // GET REQUEST
 function getTodos() {
-  console.log('GET Request');
+  // axios({
+  //   method: 'get',
+  //   url: 'https://jsonplaceholder.typicode.com/todos',
+  //   Params: {
+  //     _limit: 5
+  //   }
+  //  })
+  //    .then(res => showOutput(res))
+  //    .catch(err => console.log(err));
+
+     axios
+     .get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+       .then(res => showOutput(res))
+       .catch(err => console.log(err)); 
 }
 
 // POST REQUEST
 function addTodo() {
-  console.log('POST Request');
+  axios.post('https://jsonplaceholder.typicode.com/todos',{
+      title: 'New Todo',
+      completed: false
+  })
+       .then(res => showOutput(res))
+       .catch(err => console.log(err));
+
 }
 
 // PUT/PATCH REQUEST
 function updateTodo() {
-  console.log('PUT/PATCH Request');
+  axios
+  .patch('https://jsonplaceholder.typicode.com/todos/1',{
+      title: 'update Todo',
+      completed: true
+  })
+       .then(res => showOutput(res))
+       .catch(err => console.log(err));
 }
 
 // DELETE REQUEST
 function removeTodo() {
-  console.log('DELETE Request');
+  axios
+  .delete('https://jsonplaceholder.typicode.com/todos/1')
+       .then(res => showOutput(res))
+       .catch(err => console.log(err));
 }
 
 // SIMULTANEOUS DATA
 function getData() {
-  console.log('Simultaneous Request');
+  axios.all([
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5'),
+    axios.get('https://jsonplaceholder.typicode.com/posts?_limit=5')
+  ])
+  .then(axios.spread((todos,posts)=> showOutput(posts)))
+  .catch(err => console.log(err));
 }
 
 // CUSTOM HEADERS
 function customHeaders() {
-  console.log('Custom Headers');
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'sometoken'
+    }
+  };
+  axios.post('https://jsonplaceholder.typicode.com/todos',{
+    title: 'New Todo',
+    completed: false
+}, 
+  config
+)
+     .then(res => showOutput(res))
+     .catch(err => console.log(err));
 }
 
 // TRANSFORMING REQUESTS & RESPONSES
 function transformResponse() {
-  console.log('Transform Response');
+  const options = {
+    method: 'post',
+    url: 'https://jsonplaceholder.typicode.com/todos',
+    data: {
+      title: 'Hello World'
+    },
+    transformResponse: axios.defaults.transformResponse.concat(data => {
+      data.title=data.title.toUpperCase();
+      return data;
+    })
+  };
+  axios(options).then(res => showOutput(res));
 }
 
 // ERROR HANDLING
 function errorHandling() {
-  console.log('Error Handling');
+  axios
+  .get('https://jsonplaceholder.typicode.com/todoss', {
+    // validateStatus: function(status){
+    //     return status < 500; // Reject only if status is greater or equal to 500
+    // }
+  })
+    .then(res => showOutput(res))
+    .catch(err => {
+      if(err.response){
+        //server responded with a status other than 200 range
+        console.log(err.response.data)
+        console.log(err.response.status)
+        console.log(err.response.headers);
+
+        if(err.response.status === 404){
+          alert('Error: Page Not Found');
+        }
+      }
+    }); 
 }
 
 // CANCEL TOKEN
 function cancelToken() {
-  console.log('Cancel Token');
+  const source =axios.CancelToken.source();
+  axios.get('https://jsonplaceholder.typicode.com/todos',{
+    cancelToken:source.token,
+  })
+  .then((res)=>showOutput((res)))
+  .catch((thrown)=>{
+    if(axios.isCancel(thrown)){
+      console.log('Request canceled', thrown.message);
+    }
+  })
+  if(true){
+    source.cancel('Request canceled!');
+  }
 }
 
 // INTERCEPTING REQUESTS & RESPONSES
+axios.interceptors.request.use(
+  config => {
+    console.log(
+      `${config.method.toUpperCase()} request sent to ${
+        config.url
+      } at ${new Date().getTime()}`
+    );
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
 
 // AXIOS INSTANCES
 
@@ -53,7 +159,6 @@ function showOutput(res) {
   <div class="card card-body mb-4">
     <h5>Status: ${res.status}</h5>
   </div>
-
   <div class="card mt-3">
     <div class="card-header">
       Headers
@@ -62,7 +167,6 @@ function showOutput(res) {
       <pre>${JSON.stringify(res.headers, null, 2)}</pre>
     </div>
   </div>
-
   <div class="card mt-3">
     <div class="card-header">
       Data
@@ -71,7 +175,6 @@ function showOutput(res) {
       <pre>${JSON.stringify(res.data, null, 2)}</pre>
     </div>
   </div>
-
   <div class="card mt-3">
     <div class="card-header">
       Config
